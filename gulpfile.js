b1fc54903237887109f766a436ff63f35f9c263c
John Diego <jadiego@uw.edu>
2016-07-13 13:53:13 -0700

    Added twitter widgets

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// Currently, live reload is not working php files and css files are not injecting.
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+// Browser-sync task for starting the server
+gulp.task('browser-sync', function() {
+	//Initialize browsersync
+	browserSync.init({
+		//Browsersync with a PHP server
+		proxy: "localhost:9000",
+		notify: false,
+		port: 9000
+	});
+
+	gulp.watch("./sass/**/*.scss", ['sass'])
+	gulp.watch("./**/*.php").on("change", reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+	return gulp.src("./sass/*.scss")
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest("./css"))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('default', ['sass', 'browser-sync']);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 (function() {
-	
+	"use strict";
+
+	var myApp = angular.module('myApp', [])
+
+	.controller('appCtrl', function($scope, $http) {
+		$http.get('twitter.php?q=adidasalerts')
+			.then(function(dat) {
+				console.log(dat);
+		});
+	})
 })();
